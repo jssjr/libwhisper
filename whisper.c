1fa0f7a6a09f46b094670832e0d9884f17890cd3
Scott Sanders <scott@jssjr.com>
2015-03-23 01:14:18 -0400

    Start moving things into a header file

--- whisper.c
+++ whisper.c
@@ -1,43 +1,4 @@
-#ifndef __LIBWHISPER__
-#define __LIBWHISPER__
-
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define WSP_MAX_ARCHIVES 8
-#define WSP_DATAPOINT_SIZE 12
-#define WSP_HEADER_SIZE 16
-#define WSP_ARCHIVE_INFO_SIZE 12
-
-#include <endian.h>
-
-#if __BYTE_ORDER == __BIG_ENDIAN
-/* No translation needed for big endian system */
-#define Swap2Bytes(val) val
-#define Swap4Bytes(val) val
-#define Swap8Bytes(val) val
-#else
-/* Swap 2 byte, 16 bit values: */
-
-#define Swap2Bytes(val) \
- ( (((val) >> 8) & 0x00FF) | (((val) << 8) & 0xFF00) )
-
-/* Swap 4 byte, 32 bit values: */
-
-#define Swap4Bytes(val) \
- ( (((val) >> 24) & 0x000000FF) | (((val) >>  8) & 0x0000FF00) | \
-   (((val) <<  8) & 0x00FF0000) | (((val) << 24) & 0xFF000000) )
-
-/* Swap 8 byte, 64 bit values: */
-
-#define Swap8Bytes(val) \
- ( (((val) >> 56) & 0x00000000000000FF) | (((val) >> 40) & 0x000000000000FF00) | \
-   (((val) >> 24) & 0x0000000000FF0000) | (((val) >>  8) & 0x00000000FF000000) | \
-   (((val) <<  8) & 0x000000FF00000000) | (((val) << 24) & 0x0000FF0000000000) | \
-   (((val) << 40) & 0x00FF000000000000) | (((val) << 56) & 0xFF00000000000000) )
-#endif
+#include "whisper.h"
 
 struct wsp_timeseries {
   time_t from;
@@ -346,5 +307,3 @@ int main(int argc, char **argv) {
 
   return 0;
 }
-
-#endif
