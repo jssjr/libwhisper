4c194ed8141237a82eb37600f28410b3d3677634
Scott Sanders <scott@jssjr.com>
2015-03-23 01:02:16 -0400

    checkpoint

--- whisper.c
+++ whisper.c
@@ -11,9 +11,39 @@
 #define WSP_HEADER_SIZE 16
 #define WSP_ARCHIVE_INFO_SIZE 12
 
-struct wsp_datapoint {
-  time_t timestamp;
-  float value;
+#include <endian.h>
+
+#if __BYTE_ORDER == __BIG_ENDIAN
+/* No translation needed for big endian system */
+#define Swap2Bytes(val) val
+#define Swap4Bytes(val) val
+#define Swap8Bytes(val) val
+#else
+/* Swap 2 byte, 16 bit values: */
+
+#define Swap2Bytes(val) \
+ ( (((val) >> 8) & 0x00FF) | (((val) << 8) & 0xFF00) )
+
+/* Swap 4 byte, 32 bit values: */
+
+#define Swap4Bytes(val) \
+ ( (((val) >> 24) & 0x000000FF) | (((val) >>  8) & 0x0000FF00) | \
+   (((val) <<  8) & 0x00FF0000) | (((val) << 24) & 0xFF000000) )
+
+/* Swap 8 byte, 64 bit values: */
+
+#define Swap8Bytes(val) \
+ ( (((val) >> 56) & 0x00000000000000FF) | (((val) >> 40) & 0x000000000000FF00) | \
+   (((val) >> 24) & 0x0000000000FF0000) | (((val) >>  8) & 0x00000000FF000000) | \
+   (((val) <<  8) & 0x000000FF00000000) | (((val) << 24) & 0x0000FF0000000000) | \
+   (((val) << 40) & 0x00FF000000000000) | (((val) << 56) & 0xFF00000000000000) )
+#endif
+
+struct wsp_timeseries {
+  time_t from;
+  time_t until;
+  time_t step;
+  double *values;
 };
 
 struct wsp_archive_info {
@@ -87,35 +117,33 @@ int wsp_update_many() {
   return 0;
 }
 
-int _wsp_fetch_archive(FILE *fd, struct wsp_archive_info *archive, time_t from, time_t until) {
-  int from_interval, until_interval, base_interval;
-  int from_offset, until_offset;
-  int step;
-  int points;
-  float *values;
-  struct wsp_datapoint datapoint;
+int _wsp_fetch_archive(FILE *fd, struct wsp_archive_info *archive, time_t from, time_t until, struct wsp_timeseries *ts) {
+  long from_interval, until_interval, base_interval, current_interval;
+  long from_offset, until_offset;
+  long step;
+  long points, point_time;
   uint8_t datapoint_buf[WSP_DATAPOINT_SIZE];
-  uint8_t *buf;
+  uint8_t buf[WSP_DATAPOINT_SIZE*4096];
+  uint64_t temp;
+  int i;
+  long td, pd, bd;
 
-  from_interval = from - (from % archive->seconds_per_point) + archive->seconds_per_point;
-  until_interval = until - (until % archive->seconds_per_point) + archive->seconds_per_point;
-
-  printf("from_interval: %d\n", from_interval);
-  printf("until_interval: %d\n", until_interval);
+  from_interval = (from - (from % archive->seconds_per_point)) + archive->seconds_per_point;
+  until_interval = (until - (until % archive->seconds_per_point)) + archive->seconds_per_point;
 
   fseek(fd, archive->offset, SEEK_SET);
-  fread(&datapoint_buf, sizeof(datapoint_buf), 1, fd);
+  fread(&datapoint_buf, sizeof(uint8_t), WSP_DATAPOINT_SIZE, fd);
 
   base_interval = (datapoint_buf[3]<<0) | (datapoint_buf[2]<<8) | (datapoint_buf[1]<<16) | (datapoint_buf[0]<<24);
-
-  printf("dp interval: %lu\n", datapoint.timestamp);
-  printf("dp ts: %s", asctime(localtime(&datapoint.timestamp)));
-  printf("dp val: %f\n", datapoint.value);
 
   step = archive->seconds_per_point;
   points = (until_interval - from_interval) / step;
 
-  if ((values = calloc(points, sizeof(float))) == NULL) {
+  ts->from = from_interval;
+  ts->until = until_interval;
+  ts->step = step;
+
+  if ((ts->values = calloc(points, sizeof(double))) == NULL) {
     return -1;
   }
 
@@ -124,42 +152,58 @@ int _wsp_fetch_archive(FILE *fd, struct wsp_archive_info *archive, time_t from, 
     return 0;
   }
 
-  /* time distance = interval - base interval
-   * point distance = time distance / archive seconds per point
-   * byte distance = point distance * datapoint size
-   * offset = archive offset + ( byte distance % archive size)
-   */
-  from_offset = archive->offset + \
-                ((((from_interval - base_interval) / archive->seconds_per_point) * WSP_DATAPOINT_SIZE) % archive->size);
-  until_offset = archive->offset + \
-                ((((until_interval - base_interval) / archive->seconds_per_point) * WSP_DATAPOINT_SIZE) % archive->size);
+  td = from_interval - base_interval;
+  pd = td / archive->seconds_per_point;
+  bd = pd * WSP_DATAPOINT_SIZE;
+  from_offset = archive->offset + (bd % archive->size);
 
-  if ((buf = calloc(points, WSP_DATAPOINT_SIZE)) == NULL) {
+  td = until_interval - base_interval;
+  pd = td / archive->seconds_per_point;
+  bd = pd * WSP_DATAPOINT_SIZE;
+  until_offset = archive->offset + (bd % archive->size);
+
+  if (fseek(fd, from_offset, SEEK_SET) == -1) {
     return -1;
   }
-
-  fseek(fd, from_offset, SEEK_SET);
   if (from_offset < until_offset) {
     /* we don't wrap around the archive */
-    fread(&buf, WSP_DATAPOINT_SIZE, until_offset - from_offset, fd);
+    if ((fread(&buf, sizeof(uint8_t), until_offset - from_offset, fd)) == 0) {
+      return -1;
+    }
   } else {
     /* we wrap around the archive and need second read */
-    fread(&buf, WSP_DATAPOINT_SIZE, (archive->offset + archive->size) - from_offset, fd);
-    fseek(fd, archive->offset, SEEK_SET);
-    fread(&buf + (archive->offset + archive->size) - from_offset, WSP_DATAPOINT_SIZE, \
+    fread(&buf, sizeof(uint8_t), ((archive->offset + archive->size) - from_offset), fd);
+    if (fseek(fd, archive->offset, SEEK_SET) == -1) {
+      return -1;
+    }
+    fread(&buf[(archive->offset + archive->size) - from_offset], sizeof(uint8_t), \
         until_offset - archive->offset, fd);
   }
 
-  /* placeholder */
-
-  printf("step: %d\n", step);
-  printf("points: %d\n", points);
-  printf("values: %f\n", values[0]);
+  current_interval = from_interval;
+  for (i=0;i<points;i++) {
+    point_time = (buf[3+(WSP_DATAPOINT_SIZE*i)]<<0) | \
+                 (buf[2+(WSP_DATAPOINT_SIZE*i)]<<8) | \
+                 (buf[1+(WSP_DATAPOINT_SIZE*i)]<<16) | \
+                 (buf[0+(WSP_DATAPOINT_SIZE*i)]<<24);
+    if (point_time == current_interval) {
+      temp = ((uint64_t)(buf[11+(WSP_DATAPOINT_SIZE*i)])<<0) | \
+             ((uint64_t)(buf[10+(WSP_DATAPOINT_SIZE*i)])<<8) | \
+             ((uint64_t)(buf[9+(WSP_DATAPOINT_SIZE*i)])<<16) | \
+             ((uint64_t)(buf[8+(WSP_DATAPOINT_SIZE*i)])<<24) | \
+             ((uint64_t)(buf[7+(WSP_DATAPOINT_SIZE*i)])<<32) | \
+             ((uint64_t)(buf[6+(WSP_DATAPOINT_SIZE*i)])<<40) | \
+             ((uint64_t)(buf[5+(WSP_DATAPOINT_SIZE*i)])<<48) | \
+             ((uint64_t)(buf[4+(WSP_DATAPOINT_SIZE*i)])<<56);
+      ts->values[i] = *(double*)&temp;
+    }
+    current_interval += step;
+  }
 
   return 0;
 }
 
-int wsp_fetch(char *path, time_t from, time_t until) {
+int wsp_fetch(char *path, time_t from, time_t until, struct wsp_timeseries *ts) {
   FILE *wsp_file;
   struct wsp_header header;
   time_t now;
@@ -210,7 +254,7 @@ int wsp_fetch(char *path, time_t from, time_t until) {
   }
 
   printf("using archive %d\n", i);
-  _wsp_fetch_archive(wsp_file, &header.archives[i], from, until);
+  _wsp_fetch_archive(wsp_file, &header.archives[i], from, until, ts);
 
   fclose(wsp_file);
 
@@ -241,11 +285,13 @@ int wsp_validate_archive_list() {
 int main(int argc, char **argv) {
   FILE *my_file;
   struct wsp_header header;
+  struct wsp_timeseries ts;
   char *filename = "test/mem-free.wsp";
   int i;
 
   time_t from;
   time_t until;
+  time_t pos;
 
   printf("whisper info for %s\n", filename);
   my_file = fopen(filename, "rb");
@@ -271,15 +317,32 @@ int main(int argc, char **argv) {
   fclose(my_file);
 
 
+  /*
   from = 0;
   time(&until);
-  wsp_fetch(filename, from, until);
+  wsp_fetch(filename, from, until, &ts);
+  for (i = 0, pos = ts.from ; pos < ts.until ; pos += ts.step, i++) {
+    printf("%d\t%f\n", (int)pos, ts.values[i]);
+  }
+  */
 
 
   time(&from);
   from = from - 3600;
   time(&until);
-  wsp_fetch(filename, from, until);
+  wsp_fetch(filename, from, until, &ts);
+  for (i = 0, pos = ts.from ; pos < ts.until ; pos += ts.step, i++) {
+    printf("%d\t%f\n", (int)pos, ts.values[i]);
+  }
+  free(ts.values);
+
+
+  wsp_fetch(filename, 1427083080, 1427083220, &ts);
+  for (i = 0, pos = ts.from ; pos < ts.until ; pos += ts.step, i++) {
+    printf("%d\t%f\n", (int)pos, ts.values[i]);
+  }
+  free(ts.values);
+
 
   return 0;
 }
